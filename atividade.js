9e0f105d617148f53d4e8b8e8baace2788b004d5
Jackson0Souza <129568316+Jackson0Souza@users.noreply.github.com>
2024-04-04 19:45:45 +0000

    Commit

---
+++ atividade.js
@@ -0,0 +1,11 @@
+const soma = (a, b) => {
+    const resultado = a + b
+
+    if(resultado > 100){
+        console.log(resultado);
+    }else{
+        console.log('ERRO');
+    }
+}
+
+soma(10, 70)
